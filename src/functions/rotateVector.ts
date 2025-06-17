26a2da35b23a0b67835ecbb2b75b1f116a0fc854
CleverYeti <cleveryeti3@gmail.com>
2025-06-16 20:30:30 -0400

    removed unused files

--- src/functions/rotateVector.ts
+++
@@ -1,6 +0,0 @@
-export function rotateVector(vector: [number, number], angle: number): [number, number] {
-  const [x, y] = vector;
-  const cos = Math.cos(angle);
-  const sin = Math.sin(angle);
-  return [x * cos - y * sin, x * sin + y * cos];
-}
